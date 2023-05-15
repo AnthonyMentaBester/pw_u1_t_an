67a729eec5bd6ce80c4e151f20663f5d125fb62d
unknown <Anthony Narvaez>
2023-05-15 07:50:15 -0500

    Mi primer tarea subida

---
+++ src/main/java/com/edu/ec/tareas/PwI1TAnApplication.java
@@ -0,0 +1,21 @@
+package com.edu.ec.tareas;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwI1TAnApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwI1TAnApplication.class, args);
+	}
+	
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("mi primera tarea");
+		
+	}
+
+}
